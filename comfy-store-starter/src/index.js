f03ab303773693f6d39f122c3d9724c2380304dc
leanhdat <le_anh_dat@allgrow-labo.com.vn>
2021-04-11 20:59:07 +0700

    Finish video 350

--- comfy-store-starter/src/index.js
+++ comfy-store-starter/src/index.js
@@ -7,14 +7,24 @@ import { FilterProvider } from './context/filter_context'
 import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
-
+//dev--0cuuh5g.us.auth0.com
+//e0lrwkBqUsfxRWUVOV4QjBUzzsqeC21a
 ReactDOM.render(
-    <ProductsProvider>
-        <FilterProvider>
-            <CartProvider>
-                <App />
-            </CartProvider>
-        </FilterProvider>
-    </ProductsProvider>
+    <Auth0Provider
+        domain="dev--0cuuh5g.us.auth0.com"
+        clientId="e0lrwkBqUsfxRWUVOV4QjBUzzsqeC21a"
+        redirectUri={window.location.origin}
+        cacheLocation = 'localstorage'
+    >
+        <UserProvider>
+            <ProductsProvider>
+                <FilterProvider>
+                    <CartProvider>
+                        <App />
+                    </CartProvider>
+                </FilterProvider>
+            </ProductsProvider>
+        </UserProvider>
+    </Auth0Provider>
     , document.getElementById('root')
 )
